5e405959078017ad82ab58a24c6d458220e87a6a
IRVINPC\Irvin <marinc.irvin@gmail.com>
2022-08-11 23:57:58 -0500

    footer oculto

--- ReporteChartjs/config.js
+++ ReporteChartjs/config.js
@@ -1,4 +1,4 @@
 const gethostApi = () => {
-    // return "http://172.22.17.200:4000/api/"
-    return "http://172.22.3.65:4000/api/"
+    return "http://172.22.17.200:4000/api/"
+    // return "http://172.22.3.65:4000/api/"
 }
